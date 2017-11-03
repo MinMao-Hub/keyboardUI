906af22814988d3cbe6270b878646ecf8d751662
gyh <guoyonghong@jfpal.com>
2017-11-03 11:27:42 +0800

    create class `MMInputView`

---
+++ MMKeyBoard/MMInputView.h
@@ -0,0 +1,13 @@
+//
+//  MMInputView.h
+//  keyboardUI-iOS
+//
+//  Created by 郭永红 on 2017/11/3.
+//  Copyright © 2017年 keeponrunning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMInputView : UIInputView
+
+@end
---
+++ MMKeyBoard/MMInputView.m
@@ -0,0 +1,21 @@
+//
+//  MMInputView.m
+//  keyboardUI-iOS
+//
+//  Created by 郭永红 on 2017/11/3.
+//  Copyright © 2017年 keeponrunning. All rights reserved.
+//
+
+#import "MMInputView.h"
+
+@implementation MMInputView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
